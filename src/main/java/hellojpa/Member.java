a2d73f4149abe487daf1bbf9f031748c9fb72b6d
dlawnsrjs@gmail.com <dlawnsrjs@gmail.com>
2022-09-24 18:54:35 +0900

    9-24 Entity Manager(영속성 컨텍스트) 의 FLUSH 작동 과정과 1차캐시

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -9,11 +9,20 @@ import javax.persistence.Table;
 //@Table(name = "USER") //쿼리가 나갈때 유저 테이블에 INSERT 쿼리가 나감
 public class Member {
 
+    public Member(){ // JPA는 기본생성자가 필요함
+
+    }
+
     @Id
     private Long id;
 
 //    @Column(name = "username") //이러면 (username, id)로 쿼리가 나감
     private String name;
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public Long getId() {
         return id;
