6e1f7d847c2822e4233c0fed02188eb5cc139c0f
dlawnsrjs@gmail.com <dlawnsrjs@gmail.com>
2022-09-28 21:15:29 +0900

    9-28 JPA 기본키 매핑전략

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -7,7 +7,8 @@ import javax.persistence.Persistence;
 
 
 public class JpaMain {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); //애플리케이션 로딩시점에 딱 한개만 만들어야함 //DB당 한개
 
         EntityManager em = emf.createEntityManager(); //일관적인 단위작업을 할때마다 엔티티매니저를 만들어줘야함
@@ -18,77 +19,88 @@ public class JpaMain {
 
         //code
         try { //정상실행
-//
-//            Member findMember = em.find(Member.class, 1L); // 멤버 찾기
-//
-////            System.out.println("findMember = " + findMember.getId());   // findMember = 1
-////            System.out.println("findMember = " + findMember.getName());  // findMember = HelloA
-//
-////            em.remove(findMember); //삭제
-//            findMember.setName("HelloJPA"); //수정             em.persist(findMember); 안해도된다!
-//            // ㄴ> JPA를 통해서 엔티티를 가져오면 관리를 하게 되는데 변경여부를 트랜잭션 커밋하는 시점에 확인을 하고 변경이 확인되면 UPDATE 쿼리를 만들고 날림!
-//            //조건이 있는 조회는? JPQL로 해야한다
-//            List<Member> result = em.createQuery("SELECT m from Member as m", Member.class)
-//                    .setFirstResult(1)
-//                    .setMaxResults(10)
-//                    .getResultList();
-//
-//            for (Member member1 : result) {
-//                System.out.println("member1 = " + member1.getName());
-//            }
+/* 영속성관리 - 내부 동작 방식
+            Member findMember = em.find(Member.class, 1L); // 멤버 찾기
 
-//            Member findMember = em.find(Member.class, 101L);
-//            Member findMember2 = em.find(Member.class, 101L); //쿼리가 한개만 나감 ! //1차캐시에서 가져오기때문
+//            System.out.println("findMember = " + findMember.getId());   // findMember = 1
+//            System.out.println("findMember = " + findMember.getName());  // findMember = HelloA
 
-//            System.out.println(findMember == findMember2); // : ~ true ~ 같은 트랜잭션 내에서 영속 엔티티의 동일성 보장
+//            em.remove(findMember); //삭제
+            findMember.setName("HelloJPA"); //수정             em.persist(findMember); 안해도된다!
+            // ㄴ> JPA를 통해서 엔티티를 가져오면 관리를 하게 되는데 변경여부를 트랜잭션 커밋하는 시점에 확인을 하고 변경이 확인되면 UPDATE 쿼리를 만들고 날림!
+            //조건이 있는 조회는? JPQL로 해야한다
+            List<Member> result = em.createQuery("SELECT m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
 
-//            ==== BEFORE ====
-//            ==== AFTER ====
-            //Hibernate:
-            //    /* insert hellojpa.Member
-            //        */ insert
-            //        into
-            //            Member
-            //            (name, id)
-            //        values
-            //            (?, ?)
-//
-//            Member member1 = new Member(150L, "A");
-//            Member member2 = new Member(160L, "B");
-//
-//            em.persist(member1);
-//            em.persist(member2);  //차곡차곡 엔티티도 쌓이고 쿼리도 쌓인다 . 쿼리를 쓸때마다 커밋된다면 최적화를 할 여지가 없는데 , 이 타이밍에 버퍼링을 사용가능한 이점을 가질수있다
-//            System.out.println("==============쿼리확인 선==============="); //선 이후에 쿼리가 나가는걸 확인할 수 있다
+            for (Member member1 : result) {
+                System.out.println("member1 = " + member1.getName());
+            }
 
-            //더티체킹(변경감지)
+            Member findMember = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L); //쿼리가 한개만 나감 ! //1차캐시에서 가져오기때문
+
+            System.out.println(findMember == findMember2); // : ~ true ~ 같은 트랜잭션 내에서 영속 엔티티의 동일성 보장
+
+            ==== BEFORE ====
+            ==== AFTER ====
+            Hibernate:
+                 insert hellojpa.Member
+                     insert
+                    into
+                        Member
+                        (name, id)
+                    values
+                        (?, ?)
+
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
+
+            em.persist(member1);
+            em.persist(member2);  //차곡차곡 엔티티도 쌓이고 쿼리도 쌓인다 . 쿼리를 쓸때마다 커밋된다면 최적화를 할 여지가 없는데 , 이 타이밍에 버퍼링을 사용가능한 이점을 가질수있다
+            System.out.println("==============쿼리확인 선==============="); //선 이후에 쿼리가 나가는걸 확인할 수 있다
+
+            더티체킹(변경감지)
 
 
-//            Member member = new Member(203L, "member203");
-//            em.persist(member);
+            Member member = new Member(203L, "member203");
+            em.persist(member);
 
-            //쿼리를 미리 보고싶다 (강제호출)
+            쿼리를 미리 보고싶다 (강제호출)
+            em.flush(); //쿼리를 바로 반영 !! 1차캐시는 그대로 다 유지됩니다.
+              변경감지란 DB트랜잭션을 커밋하는 시점에 내부적으로 FLUSH 라는게 호출되는데 1차캐시안에 있는 엔티티와 스냅샷을 비교한다. 최초로 읽어온(최초로 1차캐시에 들어온 엔티티)값을 스냅샷에 저장해놓고
+              DB트랜잭션 을 커밋할때 FLUSH가 호출되는 시점에 JPA 가 엔티티와 스냅샷을 비교함. 바뀐값이 있다면 UPDATE 쿼리를 쓰지지연SQL저장소에 만들어주고나서 UPDATE쿼리를 데이터베이스에 반영하고 커밋함 !
+              JPQL 쿼리 실행시 FLUSH 가 자동으로 호출이 된다. "실수로라도 쿼리가 반영되지않았을 때를 대비해서 "
 
-//            em.flush(); //쿼리를 바로 반영 !! 1차캐시는 그대로 다 유지됩니다.
-            //변경감지란 DB트랜잭션을 커밋하는 시점에 내부적으로 FLUSH라는게 호출되는데 1차캐시안에 있는 엔티티와 스냅샷을 비교한다. 최초로 읽어온(최초로 1차캐시에 들어온 엔티티)값을 스냅샷에 저장해놓고
-            //  DB트랜잭션 을 커밋할때 FLUSH가 호출되는 시점에 JPA가 엔티티와 스냅샷을 비교함. 바뀐값이 있다면 UPDATE쿼리를 쓰지지연SQL저장소에 만들어주고나서 UPDATE쿼리를 데이터베이스에 반영하고 커밋함 !
-            // JPQL 쿼리 실행시 FLUSH가 자동으로 호출이 된다. "실수로라도 쿼리가 반영되지않았을 때를 대비해서 "
+            em.detach(member); //영속성 컨텍스트(논리적인 개념)에서 제외시키기 ( 준영속 상태, detach 된 상태 )
+            em.clear();//entity manager를통해 영속성 컨텍스트 안에있는 모든걸 제외시킴 , 1차캐시를 모두 비움
 
-//            em.detach(member); //영속성 컨텍스트(논리적인 개념)에서 제외시키기 ( 준영속 상태, detach 된 상태 )
-//            em.clear();//entity manager를통해 영속성 컨텍스트 안에있는 모든걸 제외시킴 , 1차캐시를 모두 비움
+            System.out.println("==============================");
+            tx.commit(); //DB에 반영 필수 !! // 트랜잭션 커밋시점에  쓰지지연 SQL 저장소에 있던 엔티티들이 FLUSH되면서 쿼리가 날아감! 이후 실제 데이터베이스 트랜잭션에 커밋
+            트랜잭션이 커밋되면 FLUSH가 무조건 발생함 이때 변경감지가 일어나고ㅡ 수정된 엔티티를 쓰기지연  SQL저장소에 등록함, 쓰기지연SQL저장소의 쿼리를 데이터베이스에 전송(등록,수정,삭제 쿼리).
+            FLUSH가 발생한다고해서 트랜잭션이 커밋되는게 아니고 쿼리를 보낸후 커밋됨
 
-//            System.out.println("==============================");
-//            tx.commit(); //DB에 반영 필수 !! // 트랜잭션 커밋시점에  쓰지지연 SQL 저장소에 있던 엔티티들이 FLUSH되면서 쿼리가 날아감! 이후 실제 데이터베이스 트랜잭션에 커밋
-            //트랜잭션이 커밋되면 FLUSH가 무조건 발생함 이때 변경감지가 일어나고ㅡ 수정된 엔티티를 쓰기지연  SQL저장소에 등록함, 쓰기지연SQL저장소의 쿼리를 데이터베이스에 전송(등록,수정,삭제 쿼리).
-            //FLUSH가 발생한다고해서 트랜잭션이 커밋되는게 아니고 쿼리를 보낸후 커밋됨
+            직접 영속성 컨텍스트를 플러시하는방법
 
-            //직접 영속성 컨텍스트를 플러시하는방법
+ */
+            /*
+            * 엔티티 매핑 - 기본 키 매핑
+            * */
+
+            Member member = new Member();
+//            member.setId("ID_A"); //직접 ID를 만들어서 사용한다면
+            member.setUsername("C");
+
+            em.persist(member);
+
             tx.commit(); //자동으로 호출
         } catch (Exception e) { //에러나면 롤백
-            System.out.println("==========에러떴대요===========");
+            System.out.println("==========에러발생===========");
             tx.rollback();
         }
         finally { //다쓰고나서 무조건 엔티티 매니저 닫기
-            System.out.println("==========엔티티매니저 퇴근===========");
+            System.out.println("==========엔티티매니저 종료===========");
             em.close();
         }
 
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -9,19 +9,25 @@ import java.util.Date;
 //하지만 테이블을 다른이름으로 사용하고싶다면?
 //@Table(name = "MBR")// 이렇게 하면 MBR 이라는 테이블과 매핑, 인자로 카탈로그, 스키마 매핑가능, 유니크제약조건도 가능
 //@Table(uniqueConstraints = )// 유니크제약조건은 이 방식이 더 선호됨.
+//@SequenceGenerator(
+//        name = "MEMBER_SEQ_GENERATOR",
+//        sequenceName = "MEMBER_SEQ", //매핑할 DB시퀀스 이름
+//        initialValue = 1, allocationSize = 50) // initialValue = DDL생성시에만 사용됨  처음 시작하는 수를 지정한다,
+//        allocationSize = 시퀀스 한번 호출에 증가하는 수 (성능 최적화에 사용됨) 매번 호출하면 네트워크를 계속 쓰게되니 미리 50개를 DB에 먼저 올려놓고 메모리에서 1씩  사용. <- 반복.
 public class Member {
+/*엔티티 매핑 - 필드와 컬럼 매핑
     @Id //(PK 매핑
     private Long id;
 
-    @Column(name = "username",updatable = true,nullable = false) //객체는 name이라고 쓰고싶은데 DB에는 username이라고 써야한다면 이렇게 작성 (데이터베이스 컬럼명은 username이야 //insertable 컬럼을 수정했을때 인서트를 할꺼야 말꺼야 , updatable update문이 나갈때 해당컬럼을 변경 할거야말거야~
+    @Column(name = "username",updatable = true,nullable = false) //객체는 name 이라고 쓰고싶은데 DB 에는 username 이라고 써야한다면 이렇게 작성 (데이터베이스 컬럼명은 username이야 //insertable 컬럼을 수정했을때 인서트를 할꺼야 말꺼야 , updatable update문이 나갈때 해당컬럼을 변경 할거야말거야~
     private String name; //등록하고 변경은 절대하면안된다? updatable을 false로 두면 절대로 변경되지않는다. DB에서 강제로 업데이트하면 바뀌긴하지만 어플리케이션에선 바뀌지않음.
     //nullable Null 제약조건이 붙는다,
-    private Integer age; //DB에도 숫자타입으로 지정해짐
+    private Integer age; //DB 에도 숫자타입으로 지정해짐
 
     @Enumerated(EnumType.STRING) //enum타입을 쓰고싶을때 사용 기본이 ORDINAL인데 쓰면 안됨!! 왜냐면 변경사항이 추가되었을때 바뀌지 않기때문!! 0(user) ,1(ADMIN), 0(GUEST *추가됨) 으로 나오기때문.
     private RoleType roleType;
 
-    @Temporal(TemporalType.TIMESTAMP) //생성일자. DATE, TIME, TIMESTAMP(날짜시간 두개다포함) JAVA8  이후  LocalDate와 localDateTime이 들어온이후 잘 쓰지않는 어노테이션
+    @Temporal(TemporalType.TIMESTAMP) //생성일자. DATE, TIME, TIMESTAMP(날짜시간 두개다포함) JAVA8  이후  LocalDate 와 localDateTime 이 들어온이후 잘 쓰지않는 어노테이션
     private Date createDate;
 
     @Temporal(TemporalType.TIMESTAMP)//DATE, TIME, TIMESTAMP(날짜시간 두개다포함)
@@ -32,8 +38,48 @@ public class Member {
 
     @Transient// 뭔가 메모리에서만 임시로 쓰고싶다, 캐시데이터를 넣어둔다던지 DB 신경쓰고싶지않아..이럴때 사용
     private int temp; //
+*/
+    /*
+    *엔티티매핑 - 기본키 매핑
+    * 기본 키 (PK) 제약 조건 : null 아님, 유일, 변하면안된다(서비스의 전체기간 9억년동안 변하면 안된다.)
+    * 9억년 까지 이 조건을 만족하는 자연키(Nature key :비즈니스적으로 의미있는 키 : 전화번호, 주민번호)를 찾기 어렵다. 대리키(대체키 : 비즈니스와 전혀 상관없는 키)를 사용하자.
+    * 주민등록번호도 기본 키로 적절하지 않다.(ex: 정부: 주민번호 보관하지 마세요, 기업 : 헉 우리 PK 주민번호 인데 . ex2: 회원을 갖다쓴 나머지 테이블도 FK로 PK(주민번호)들고있었을텐데...그러면 난리나겠죠 ??)
+    * 권장 : Long 자료형 + 대체키(시퀀스, UUID) + 키 생성전략 을 조합해서 사용(결론: Auto increment, sequence object 둘중에 하나를 쓰던가 ..회사내의 룰대로..※ 절대로 자연키를 PK로 끌고오지말것 !
+    */
 
-    public Member(){ // JPA는 기본생성자가 필요함
+//    @Id //직접할당은 @id, 자동생성은 (@GeneratedValue) DB 별로 다름
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //자동생성/ 뭔가 값을 생성하고싶다 GenerationType.AUTO 옵션은 DB 방언 별로 적용 (확인해보고 쓰자)
+//    generator = "MEMBER_SEQ_GENERATOR")  //@SequenceGenerator
 
+    //GenerationType.IDENTITY 전략 (strategy): MySQL 에서 사용 기본 키 생성을 데이터 베이스에 위임한다 . 이 전략은 내가 ID에 값을 넣으면안되고(쿼리는 null 로 insert 날아감) DB에 insert 를 해야한다, ==DB에 값이 들어가야 ID값을 알수있다..
+    //근데 jpa 는 Entity manager(영속성 컨텍스트)에서 관리되려면 무조건 PK 값이 있어야한다.근데 DB에 넣기전까지 PK 값을 모르는데..?? 이게 무슨 모순 ??
+    //JPA 의 울며겨자먹기 해결방법::이 IDENTITY 전략에서만 특수하게 em.persist 를 호출한시점에 바로 INSERT 쿼리가 날아감(원래는 commit 시점에 날아가지만..)
+
+    //GenerationType.SEQUENCE 전략 (strategy) : 주로 Oracle 데이터베이스에서 사용 . SEQUENCE  전략도 마찬가지로 Entity Manager 가 MEMBER_SEQ 에서 PK 값을 끌어옴 그 후에 영속성 컨텍스트에 저장, 쿼리는 날아가지 않음
+    private Long id; //Long 인 이유 : int 의 경우 10억이 넘어가면 다시 1부터인데 그때 다시 바꾸느니 차라리 처음부터 Long 을 써라
+
+    @Column(name = "name", nullable = false)
+    private String username;
+
+    public Member() { // JPA는 기본생성자가 필요함
+
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
